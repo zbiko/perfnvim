2617278bc13aeb11052c41cfaf4b6948b719117a
zkondolewicz <zkondolewicz@nvidia.com>
2024-12-04 15:11:01 +0100

    Verify if there are signs on a list before signing lines

--- lua/perfnvim/commands.lua
+++ lua/perfnvim/commands.lua
@@ -216,7 +216,7 @@ function M.GoToPreviousChange()
         end
     end
 
-    if(not p4signs) then
+    if(#p4signs == 0) then
         return
     end
 
@@ -255,7 +255,7 @@ function M.GoToNextChange()
         end
     end
 
-    if(not p4signs) then
+    if(not p4signs == 0) then
         return
     end
 
